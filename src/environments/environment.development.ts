f142df66fe0ed36fdb04056d445df87ac19b88f4
aaya-ahmed <aaya7098@gmail.com>
2023-06-06 00:32:21 +0300

    update environment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,7 @@
 export const environment = {
-    URL:"https://localhost:44309/api/",
-    imgeurl:"https://localhost:44309/",
-    email:"smartschoolcomplaints@gmail.com"
-
+     // URL:"https://localhost:44309/api/",
+     URL:"http://ahmedelkadafy-001-site1.atempurl.com/api/",
+     // imgeurl:"https://localhost:44309/",
+     imgeurl:"http://ahmedelkadafy-001-site1.atempurl.com/",
+     email:"smartschoolcomplaints@gmail.com"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,8 @@
 export const environment = {
-     URL:"https://localhost:44309/api/",
-     imgeurl:"https://localhost:44309/",
+     // URL:"https://localhost:44309/api/",
+     URL:"http://ahmedelkadafy-001-site1.atempurl.com/api/",
+     // imgeurl:"https://localhost:44309/",
+     imgeurl:"http://ahmedelkadafy-001-site1.atempurl.com/",
      email:"smartschoolcomplaints@gmail.com"
 
 };
