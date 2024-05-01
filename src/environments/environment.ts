5fe66a27cc6092fa5b7d734ac3cc3e8381eb3d0c
aya ahmed <91829518+aaya-ahmed@users.noreply.github.com>
2024-05-01 22:38:10 +0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
-     URL:"https://smartschool99.somee.com/api/",
-     imgeurl:"https://smartschool99.somee.com/",
+     URL:"https://smartschool.somee.com/api/",
+     imgeurl:"https://smartschool.somee.com/",
      email:"smartschoolcomplaints@gmail.com"
 
 };
